d7a7d2a70cf018a2c22177f07020212cc041dc13
idoviron <idoviron@gmail.com>
2022-06-29 20:54:52 +0300

    Updated globe animation

--- Kama_Ze_Ole_Tst/Assets/Scripts/DrageMode.cs
+++ Kama_Ze_Ole_Tst/Assets/Scripts/DrageMode.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public enum Inputs { Drag, Click, MultiTouch, FakeTouch }
 public class DrageMode : MonoBehaviour
@@ -12,6 +13,57 @@ public class DrageMode : MonoBehaviour
     [SerializeField] internal ShoppingCart shoppingCartBL;
     [SerializeField] internal ShoppingCart shoppingCartBR;
     //internal static int PressedScreen = 0;
+    private void Start()
+    {
+        if (Directory.Exists("c:\\media"))
+        {
+            if (File.Exists("c:\\media\\dragmode.ini"))
+            {
+                string dragModeFromFile = File.ReadAllText("c:\\media\\dragmode.ini");
+                if (dragModeFromFile == "A")
+                {
+                    print("drag mode");
+                    _input = Inputs.Drag;
+                    inputs = Inputs.Drag;
+                }
+                else if (dragModeFromFile == "B")
+                {
+                    print("click mode");
+                    _input = Inputs.Click;
+                    inputs = Inputs.Click;
+                }
+                else if (dragModeFromFile == "C")
+                {
+                    print("multi touch mode");
+                    _input = Inputs.MultiTouch;
+                    inputs = Inputs.MultiTouch;
+                }
+                else if (dragModeFromFile == "D")
+                {
+                    print("fake touch mode");
+                    _input = Inputs.FakeTouch;
+                    inputs = Inputs.FakeTouch;
+                }
+                switch (inputs)
+                {
+                    case Inputs.Drag:
+                        _input = Inputs.Drag;
+                        break;
+                    case Inputs.Click:
+                        _input = Inputs.Click;
+                        break;
+                    case Inputs.MultiTouch:
+                        _input = Inputs.MultiTouch;
+                        break;
+                    case Inputs.FakeTouch:
+                        _input = Inputs.FakeTouch;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
 
     private void Update()
     {
